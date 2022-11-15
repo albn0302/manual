92f667e9c3389832e2ac223716fcb3048b3c0480
albn0302 <albin.johansson0302@gmail.com>
2022-11-15 10:26:46 +0200

    Javascript Manual

---
+++ logical_operators.js
@@ -0,0 +1,40 @@
+// Logiska Operatörer
+
+// OR (||)
+
+/* result = a || b;
+
+if (a || b) {
+    ...
+}; */
+
+/* let hour = 9;
+let isWeekend = true;
+
+if (hour < 10 || hour > 18) {
+    alert('Butiken är stängd.');
+}; */
+
+// AND (&&)
+
+/* result = a && b;
+
+let hour = 12;
+let minute = 30;
+
+if (hour == 12 && minute == 30) {
+    alert('The time is 12.30.');
+}; */
+
+// NOT (!)
+
+// 1. Konverterar operanden till typen boolean: sant/falskt
+// 2. Returnerar det inverterade värdet
+
+result = !value;
+
+alert(!true); // Falskt
+
+// 0 är alltid falskt
+alert(!0); // Sant
+
