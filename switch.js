ce12a53a3728c5a9102ad63769aafdad00f69e91
albn0302 <albin.johansson0302@gmail.com>
2022-11-15 10:48:10 +0200

    Added switch

---
+++ switch.js
@@ -0,0 +1,43 @@
+// "Switch" påståendet
+
+// Ett "switch" påståendet kan byta ut flera "if" kollar
+
+/* let a = 2 + 2;
+
+switch (a) {
+    case 3:
+        alert('För liten.');
+        break;
+    case 4:
+        alert('Exakt!');
+        break;
+    case 5:
+        alert('För stor.');
+        break;
+    default:
+        alert('Jag kan inte sådana värden.');
+}; */
+
+/* let browser = "Brave";
+
+switch (browser) {
+    case "Edge":
+        alert('La de jaan, int naan tycker om de.');
+        break;
+    case "Chrome":
+    case "Firefox":
+    case "Opera":
+        alert('Eyy, grattis! Du e nu en respektabel peson.');
+        break;
+    default:
+        alert('Ja hoppas att denhär sido siir bra ut, lol!');
+};
+
+// Samma med "if ... else"
+if (browser == "Edge") {
+    alert('La de jaan, int naan tycker om de.');
+} else if (browser == "Chrome" || browser == "Firefox" || browser == "Opera") {
+    alert('Eyy, grattis! Du e nu en respektabel peson.');
+} else {
+    alert('Ja hoppas att denhär sido siir bra ut, lol!');
+}; */
